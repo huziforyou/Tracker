cf431af29b3b9e4efd91fb4831c9ed07b13a4bb1
Hassam97 <hassam.pixcile@gmail.com>
2026-07-20 20:34:34 +0500

    Use FormData for Vercel Blob

--- src/components/CameraCapture.jsx
+++ src/components/CameraCapture.jsx
@@ -93,16 +93,17 @@ function CameraCapture({ onSuccess }) {
   const uploadData = async (imageData) => {
     try {
       console.log('Uploading to:', `${API_BASE_URL}/api/records`);
-      console.log('Data to upload:', {
-        selfie: imageData.substring(0, 50) + '...',
-        location,
-        browserInfo
-      });
-      const response = await axios.post(`${API_BASE_URL}/api/records`, {
-        selfie: imageData,
-        location: location,
-        browserInfo: browserInfo
-      });
+      
+      // Convert base64 to blob
+      const responseFetch = await fetch(imageData);
+      const blob = await responseFetch.blob();
+      
+      const formData = new FormData();
+      formData.append('selfie', blob, 'selfie.png');
+      formData.append('location', JSON.stringify(location));
+      formData.append('browserInfo', JSON.stringify(browserInfo));
+      
+      const response = await axios.post(`${API_BASE_URL}/api/records`, formData);
       console.log('Upload response:', response.data);
       setStatus('success');
       setTimeout(onSuccess, 2000);
